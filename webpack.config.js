e5a6d566e7442bda2aec46dacb141e32934a820c
Sean Gregory <seangregory@gmail.com>
2018-04-22 19:50:24 -0400

    init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+module.exports = {
+  entry: {
+		site: "./src/site.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "site/client"),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /\.babelrc/,
+        use: [ 'vue-loader' ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['es2015', { modules: false }]
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  externals: {
+    jquery: 'jQuery',
+    vue: 'Vue',
+  }
+};
