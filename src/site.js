c7dbc5b8e10fc2c78eb601770b1bf6b0b9355989
Sean Gregory <seangregory@gmail.com>
2018-04-22 21:51:58 -0400

    first lifework

--- src/site.js
+++ src/site.js
@@ -1,1 +1,33 @@
-console.log('hello world')
+import Vue from 'vue'
+import YAML from 'js-yaml'
+
+window.Site = window.Site || (function(){
+  return {
+    init(args) {
+      console.log('args', args)
+      new Vue({
+        el: args.el,
+        data: {
+          links: args.links,
+          activeLink: null,
+          lifeWorks: [],
+          activeWork: null,
+        },
+        mounted() {
+          let vm = this
+          let type = 'GET'
+          let url = 'data/life.yml'
+          let dataType = 'text'
+          let success = (yaml) => {
+            vm.lifeWorks = YAML.safeLoad(yaml)
+          }
+          $.ajax({ type, url, dataType, success })
+        },
+        computed: {
+        },
+        methods: {
+        },
+      })
+    }
+  }
+}())
